c2a2ca976c3b1b852d41e51321b67301f417d4e4
Gokul S <gokkull04@gmail.com>
2025-01-28 22:45:14 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,6 +24,7 @@ const Home = ({ connectWallet, walletAddress }) => {
   return (
     <>
       <main className="container mx-auto px-6 py-20">
+    
         <div className="text-center space-y-8">
           <motion.h1
             initial={{ opacity: 0, y: 20 }}
